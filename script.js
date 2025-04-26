3d467ce9132f21962eadd8ca9c3da559eb99f12f
George R <robsongeorge@hotmail.com>
2025-04-26 17:35:17 +1200

    add LoanCalculator class

---
+++ script.js
@@ -0,0 +1,7 @@
+class LoanCalculator {
+    constructor(loanAmount, loanRate, loanTerm){
+        this.loanAmount = loanAmount;
+        this.loanRate = loanRate;
+        this.loanTerm = loanTerm;
+    }
+}
