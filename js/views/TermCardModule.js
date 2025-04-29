3b2b863a44c43ac89a0578c603a3808ff8f44feb
George R <robsongeorge@hotmail.com>
2025-04-29 19:28:43 +1200

    copy code from script js
    not working

---
+++ js/views/TermCardModule.js
@@ -0,0 +1,48 @@
+export class TermCardModule{
+    //Handles the displaying of the terms
+    constructor(){
+        this.termCards = [];
+
+        this.bindEvents()
+    }
+
+    createNewTermCard(term){
+        let newTermCard = new TermCard(term.index);
+        newTermCard.updateContent(term)
+
+        this.termCards.push(newTermCard)
+        
+    }
+
+    updateTermCard(term){
+        let index = term.index;
+
+        this.termCards[index].updateContent(term)
+    }
+
+    
+    deleteTermAtIndex(index){
+        //Remove element from DOM
+        this.termCards[index].cacheDOM.termCard.remove();
+        
+        //remove element from array
+        this.termCards.splice(index, 1);
+
+        //update indices for each item
+        this.updateIndices()
+    }
+
+    updateIndices(){
+        this.termCards.forEach((element, index) => {
+            console.log
+            element.index = index;
+        });
+    }
+
+
+    bindEvents(){
+        events.on("CreateNewTermCard", this.createNewTermCard.bind(this))
+        events.on("DeleteTermButton", this.deleteTermAtIndex.bind(this))
+        events.on("UpdatedTerm", this.updateTermCard.bind(this))
+    }
+}
